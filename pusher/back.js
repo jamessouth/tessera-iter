63dabbb6e92c8653704ec4f646d353f5a749ce82
jamessouth <jamesdanielsouth@gmail.com>
2024-04-15 18:04:02 -0500

    json data passing

--- pusher/back.js
+++ pusher/back.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import { config } from 'dotenv';
-import * as path from 'path';
-import { fileURLToPath } from 'url';
 import cors from "cors";
+import { config } from 'dotenv';
+import express from 'express';
+import * as path from 'path';
 import Pusher from 'pusher';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
--- pusher/front.js
+++ pusher/front.js
@@ -1,5 +1,3 @@
-
-
 import Pusher from 'pusher-js';
 
 // function setCookie({ name, value, expiryInDays }) {
@@ -20,11 +18,10 @@ import Pusher from 'pusher-js';
 //   container.appendChild(eventElement);
 // }
 
- function getPusherClient() {
-    Pusher.logToConsole = true;
-    return new Pusher("fe257281aabc7cebb6f2", { cluster: "us2" }); 
-  }
-
+function getPusherClient() {
+  Pusher.logToConsole = true;
+  return new Pusher('fe257281aabc7cebb6f2', { cluster: 'us2' });
+}
 
 let channel;
 
@@ -32,11 +29,10 @@ let channel;
 // loginForm.addEventListener('submit', async (event) => {
 //   event.preventDefault();
 
-
-  //   if (!dummyUsers.find((user) => user.user_info.name === username)) {
-  //     alert('User not found');
-  //     return;
-  //   }
+//   if (!dummyUsers.find((user) => user.user_info.name === username)) {
+//     alert('User not found');
+//     return;
+//   }
 //   setCookie({ name: 'username', value: username, expiryInDays: 1 });
 //   renderMessage({
 //     query: '#username-display',
@@ -47,75 +43,77 @@ let channel;
 //   loginForm.style.display = 'none';
 //   document.querySelector('#chat-area').style.display = 'flex';
 const msgs = document.querySelector('.container');
-  // ...code continues below
-  // ...continues from above
-  //   const pusherClient = await getPusherClient();
-  const pusher =  getPusherClient();
-  channel = pusher.subscribe('private-chat-channel');
+// ...code continues below
+// ...continues from above
+//   const pusherClient = await getPusherClient();
+const pusher = getPusherClient();
+channel = pusher.subscribe('private-chat-channel');
 //   console.log(channel);
-  channel.bind('client-chat-event', (data) => {
-    if (data) {
+let socketId = null;
+pusher.connection.bind("connected", () => {
+  socketId = pusher.connection.socket_id;
+});
+channel.bind('client-chat-event', (data) => {
+  if (data) {
+    // console.log(typeof data);
+    // console.log(data.message);
+    console.log(data);
+    const p = document.createElement('p');
 
-        // console.log(typeof data);
-        // console.log(data.message);
-        console.log(data);
-        const p = document.createElement('p');
+    const newContent = document.createTextNode(data.message);
 
-        const newContent = document.createTextNode(data.message);
+    p.appendChild(newContent);
 
-    
-        p.appendChild(newContent);
-
-        msgs.insertAdjacentElement('beforeend', p);
-
-
-
+    msgs.insertAdjacentElement('beforeend', p);
 
     //   renderMessage({
     //     query: '.messages',
     //     message:  === 'string' ? data : data?.message,
     //     className: 'message',
     //   });
-    }
-  });
+  }
+});
 // });
 
 // const messageForm = document.querySelector('#message-form');
 // messageForm.addEventListener('submit', async (event) => {
 //   event.preventDefault();
-async function postBookData(id) {
-    const formData = new FormData();
-    formData.append("book_id", id);
-    formData.append("fred", true);
-    formData.append("wilma", "bart");
-  
-    const resp = await fetch("/trigger", {
-      method: "POST",
-      body: formData,
-    });
+// async function postBookData(id) {
 
-   
-  
-    return await resp();
-  }
+//   }
 //   const input = document.querySelector('#message');
 const input = document.querySelector('#data');
 
-document.querySelector('#enter').addEventListener('click', e => {
-    
-    const message = input.value;
+document.querySelector('#enter').addEventListener('click', (e) => {
+  const message = input.value;
 
-    channel.trigger('client-chat-event', {
-     message,
-    });
-    
-    input.value = '';
+  channel.trigger('client-chat-event', {
+    message,
+  });
+
+  input.value = '';
 });
-document.querySelector('#trig').addEventListener('click', async e => {
-    
-    const bookdata = await postBookData(89);
-    console.log("bd",bookdata);
+
+document.querySelector('#trig').addEventListener('click', async (e) => {
+  const bod = {
+    book_id: 87,
+    fred: true,
+    wilma: 'bart',
+    socket_id: socketId
+  };
+
+  const resp = await fetch('/trigger', {
+    method: 'POST',
+    body: JSON.stringify(bod),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  console.log('bd6666', resp);
+
+  // const bookdata =  resp();
+  // console.log("bd",bookdata);
 });
 
 // });
-
