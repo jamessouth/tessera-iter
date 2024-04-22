d45d2e946391912b6fa08e80e17b5d6ceb9a6f88
Benjamin Dorn <bkd2008@hsutx.edu>
2024-04-22 11:04:16 -0500

    Added a length attribute to Route, adjusted routes accordingly, made point value dependent on length

--- state/Route.js
+++ state/Route.js
@@ -6,15 +6,38 @@ export default class Route {
   isSmallGame = true;
   name = '';
   points = 0;
+  length = 0;
   color1 = '';
   color2 = '';
   taken1 = false;
   taken2 = false;
 
-  constructor(name, points, color1, color2 = null) {
+  constructor(name, length, color1, color2 = null) {
     this.name = name;
-    this.points = points;
+    this.length = length;
     this.color1 = color1;
     this.color2 = color2;
+
+    //Sets the points
+    switch(length){
+      case 1:
+        this.points = 1
+        break
+      case 2:
+        this.points = 2
+        break
+      case 3:
+        this.points = 4
+        break
+      case 4: 
+        this.points = 7
+        break
+      case 5:
+        this.points = 10
+        break
+      case 6:
+        this.points = 15
+        break
+    }
   }
 }
